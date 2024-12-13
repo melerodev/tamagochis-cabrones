8b8f1fa8a21ca27dc1ca1ac42bab5401e9562533
Alejandro Melero Zhohal <80626286+melerodev@users.noreply.github.com>
2024-12-13 09:52:57 +0100

    Optimiza la importación de funciones y mejora la claridad del código en check.js e index.js

--- Riego/cliente/js/check.js
+++ Riego/cliente/js/check.js
@@ -1,4 +1,5 @@
-import { Cliente } from './index.js';
+import { Cliente } from './index.js'; // para ahorra código, importo la función send de index.js para enviar los datos al servidor desde el mismo sitio donde estoy manejando los checks con eventos
+
 export class Check {
     constructor(parent,client) {
         this.parent = parent;
@@ -30,10 +31,10 @@ export class Check {
         span.appendChild(text);
         check.appendChild(span);
         input.addEventListener('change', (event)=> {
-            this.changeValue(name, event.target.checked);
-            const span = event.target.parentElement.querySelector('span');
-            span.innerHTML = event.target.checked ? 'ON' : 'OFF';
-            Cliente.send({name: event.target.closest(".form-switch").id, state: event.target.checked});
+            this.changeValue(name, event.target.checked); // cambio el valor del check
+            const span = event.target.parentElement.querySelector('span'); // obtengo el span del check
+            span.innerHTML = event.target.checked ? 'ON' : 'OFF'; // si el check está activado, el texto será ON, si no, será OFF
+            Cliente.send({name: event.target.closest(".form-switch").id, state: event.target.checked}); // envío el id del label y el estado del check al servidor
             // con "event.target.closest(".form-switch").id" obtengo el elemento más cercano que tenga el I y luego obtengo su id
         })
     }
--- Riego/cliente/js/index.js
+++ Riego/cliente/js/index.js
@@ -27,19 +27,18 @@ export const Cliente = {
 
 // cuando se cargue la página se obtengan los datos del servidor
 window.addEventListener('load', (event) => {
-  fetch('http://localhost:3000/api/items', {
-    method: 'GET',
+  fetch('http://localhost:3000/api/items', { // obtengo los datos del servidor
+    method: 'GET', // le digo al servidor que quiero obtener los datos
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json' // le digo al servidor que quiero los datos en formato JSON
     }
   })
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(item => {
-      const check = document.getElementById(item.name);
-      console.log(check);
-      check.querySelector('input').checked = item.state;
-      check.querySelector('span').innerHTML = item.state ? 'ON' : 'OFF';
+  .then(response => response.json()) // convierto los datos en JSON
+  .then(data => { // obtengo los datos
+    data.forEach(item => { // recorro los datos
+      const check = document.getElementById(item.name); // obtengo el check por su id
+      check.querySelector('input').checked = item.state; // cambio el estado del check
+      check.querySelector('span').innerHTML = item.state ? 'ON' : 'OFF'; // cambio el texto del check
     });
   })
   .catch((error) => {
@@ -58,8 +57,3 @@ check2.addCheck("riego2", 4);
 const check3 = new Check(document.getElementById("grupo3"),Cliente);
 check3.addCheck("riego1", 5);
 check3.addCheck("riego2", 6);
-
-// un nuevo grupo de checks
-const check4 = new Check(document.getElementById("grupo4"),Cliente);
-check4.addCheck("riego1", 7);
-check4.addCheck("riego2", 8);
